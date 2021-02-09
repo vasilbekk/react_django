335bb586fadf054c287071db0c7b0e47e522ab10
vasilbekk <vasilbekk@mail.ru>
2021-02-10 01:01:10 +0300

    Edit footer

--- frontend/src/layout/footer/index.jsx
+++ frontend/src/layout/footer/index.jsx
@@ -7,7 +7,7 @@ const Footer = (props) => {
         <Container fluid={true}>
           <Row>
             <Col md="12" className="footer-copyright text-center">
-              <p className="mb-0">{"Copyright 2020 © Cuba theme by pixelstrap."}</p>
+              <p className="mb-0">Powered by <a href='https://chatupper.com/' target='_blank'>«‎ChatUpper»</a> with ❤️</p>
             </Col>
           </Row>
         </Container>
