5fad72778645e6d38410e386661f20a1a57eeeed
vasilbekk <vasilbekk@mail.ru>
2021-02-20 13:54:07 +0300

    Create MENUITEMS filter, but it can be better

--- frontend/src/layout/sidebar/menu.jsx
+++ frontend/src/layout/sidebar/menu.jsx
@@ -1,5 +1,6 @@
 import {Home,Box} from 'react-feather'
 import {General, MainFunctionality} from '../../constant'
+import { isUserHavePermission } from '../../actions/user'
 export const MENUITEMS = [
     {
         menutitle:General,
@@ -12,23 +13,7 @@ export const MENUITEMS = [
                 path: `${process.env.PUBLIC_URL}/`
             }
         ]
-       
-    },
-    {
-        menutitle:"Test panel",
-        menucontent:"",
-        permission: 'view_project',
-        Items:[
-            {
-                title: 'Projects', 
-                icon: Box, 
-                type: 'link',
-                active: false,
-                path: `${process.env.PUBLIC_URL}/test`,
-                permission: 'add_test'
-                
-            }
-        ]          
+
     },
     {
         menutitle:"Admin Panel",
@@ -38,17 +23,20 @@ export const MENUITEMS = [
                 title: 'Projects', 
                 icon: Box, 
                 type: 'sub',
-                active: false, 
+                active: false,
+                permission: 'view_project',
                 children: [
                     { 
                         title: 'Create a Project', 
                         type: 'exteral_link', 
-                        path: 'http://support.pixelstrap.com/help-center'
+                        path: 'http://support.pixelstrap.com/help-center',
+                        permission: 1
                     },
                     { 
                         title: 'List of Projects', 
                         type: 'exteral_link', 
-                        path: 'http://support.pixelstrap.com/help-center'
+                        path: 'http://support.pixelstrap.com/help-center',
+                        permission: 'view_project'
                     }
                 ]
             }
@@ -58,5 +46,27 @@ export const MENUITEMS = [
 
 
 export const getMenuItemsByUser = user => {
-   return MENUITEMS
+   return MENUITEMS.filter(block => {
+    // Если у блока есть ограничение, то проверяет и возвращает, имеет приоритет надо всеми
+    if (block.permission) return isUserHavePermission(user, block.permission)
+    // Создает новый массив ссылок в блоке, отфильтрованных по ограничениям
+    var items = block.Items.filter(item => {
+        // Если у ссылки есть ограчение, проверяет
+        if (item.permission) {
+            // Если это не ссылка, а блок ссылок, то фильтрует и подссылки
+            var childrenStatus = true
+            if (item.children) {
+                // Заменяет на отфильтрованный массив подссылок
+                item.children = item.children.filter(subitem => isUserHavePermission(user, subitem.permission))
+                childrenStatus = item.children.length>0?true:false
+            }
+            return isUserHavePermission(user, item.permission) && childrenStatus
+        }
+        // Если у ссылки нет ограничений, значит можно использовать
+        return true
+    })
+    // Заменяет на отфильтрованный массив
+    block.Items = items
+    return items.length > 0
+   })
 }
